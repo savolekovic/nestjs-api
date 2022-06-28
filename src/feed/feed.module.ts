179e93e03b1bb344792dc1503d2aeaa1323eba77
Savo Lekovic <savolekovic15@gmail.com>
2022-06-28 20:04:44 +0200

    create feed controller/service

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { FeedController } from './feed.controller';
-import { FeedService } from './controllers/feed.service';
+import { FeedService } from './services/feed.service';
 import { FeedController } from './controllers/feed.controller';
 
 @Module({
