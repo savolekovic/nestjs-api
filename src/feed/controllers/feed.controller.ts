de20b2076268eb48b30aa425f50dcd0e50bdaf32
Savo Lekovic <savolekovic15@gmail.com>
2022-06-28 20:23:58 +0200

    create Post entity and interface

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('feed')
-export class FeedController {}
+export class FeedController {
+
+    @Post()
+    create(){
+        
+    }
+
+}
--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { FeedPostEntity } from '../models/post.entity';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+    constructor(
+        @InjectRepository(FeedPostEntity)
+        private readonly feedPostRepository: Repository<FeedPostEntity>
+    ){}
+
+}
