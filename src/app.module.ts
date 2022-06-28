fa03ee2c576d3197103d38f231664faf00eccfcf
Savo Lekovic <savolekovic15@gmail.com>
2022-06-28 19:57:19 +0200

    Created feed.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FeedModule } from './feed/feed.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { AppService } from './app.service';
       database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true
-    })
+    }),
+    FeedModule
   ],
   controllers: [AppController],
   providers: [AppService],
