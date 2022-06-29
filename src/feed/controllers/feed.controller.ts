e38d69f100d53f44a6588270732ab6c86cedf105
Savo Lekovic <savolekovic15@gmail.com>
2022-06-29 14:13:55 +0200

    Finished with CRUD operations

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
 
 @Controller('feed')
 export class FeedController {
+    constructor(private feedService: FeedService){}
 
     @Post()
-    create(){
-        
+    create(@Body()feedPost: FeedPost): Observable<FeedPost>{
+        return this.feedService.createPost(feedPost);
+    }
+
+    @Get()
+    findAll(){
+        return this.feedService.findAllPosts();
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id: number,
+        @Body()feedPost: FeedPost
+    ): Observable<UpdateResult>{
+        return this.feedService.updatePost(id, feedPost);
+    }
+
+    @Delete(':id')
+    delete(@Param('id')id: number): Observable<DeleteResult>{
+        return this.feedService.deletePost(id);
     }
 
 }
--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { from, Observable } from 'rxjs';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { FeedPostEntity } from '../models/post.entity';
+import { FeedPost } from '../models/post.interface';
 
 @Injectable()
 export class FeedService {
@@ -9,5 +11,21 @@ export class FeedService {
         @InjectRepository(FeedPostEntity)
         private readonly feedPostRepository: Repository<FeedPostEntity>
     ){}
+
+    createPost(feedPost: FeedPost): Observable<FeedPost> {
+        return from(this.feedPostRepository.save(feedPost));
+    }
+
+    findAllPosts(): Observable<FeedPost[]>{
+        return from(this.feedPostRepository.find());
+    }
+
+    updatePost(id: number, feedPost: FeedPost): Observable<UpdateResult>{
+        return from(this.feedPostRepository.update(id, feedPost));
+    }
+
+    deletePost(id: number): Observable<DeleteResult>{
+        return from(this.feedPostRepository.delete(id));
+    }
 
 }
