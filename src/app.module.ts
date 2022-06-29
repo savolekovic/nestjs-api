39e7181532c84b9de982e6a8c3cb5cb1ecfe4a7a
Savo Lekovic <savolekovic15@gmail.com>
2022-06-29 17:59:32 +0200

    Created auth folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeedModule } from './feed/feed.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import { FeedModule } from './feed/feed.module';
       autoLoadEntities: true,
       synchronize: true
     }),
-    FeedModule
+    FeedModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
